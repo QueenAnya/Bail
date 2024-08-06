fac719ceb3c7cca3d63c777ff1cf41dfdfb87eae
Its-me-001 <khushijain2901@mail.com>
2024-08-06 22:47:54 +0530

    @Its-me-001 #UPDATES

--- src/Types/Chat.ts
+++ src/Types/Chat.ts
@@ -4,6 +4,7 @@ import type { BufferedEventData } from './Events'
 import type { ChatLabelAssociationActionBody } from './LabelAssociation'
 import type { MessageLabelAssociationActionBody } from './LabelAssociation'
 import type { MinimalMessage } from './Message'
+import type { LabelActionBody } from './Label'
 
 /** privacy settings in WhatsApp Web */
 export type WAPrivacyValue = 'all' | 'contacts' | 'contact_blacklist' | 'none'
@@ -89,6 +90,8 @@ export type ChatModification =
     }
     | { delete: true, lastMessages: LastMessageList }
     // Label assosiation
+    // Label 🏷️
+    | { addLabel: LabelActionBody }
     | { addChatLabel: ChatLabelAssociationActionBody }
     | { removeChatLabel: ChatLabelAssociationActionBody }
     | { addMessageLabel: MessageLabelAssociationActionBody }
