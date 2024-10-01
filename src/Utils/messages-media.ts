8f40d11f0ca44656539c85ddc44ad5ad03c50893
Its-me-001 <khushijain2901@mail.com>
2024-10-01 14:46:15 +0530

    @Its-me-001 #UPDATES

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -191,7 +191,7 @@ const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
 	if (width > hight) {
 		ratio = jimp.getWidth() / 720
 	} else {
-		ratio = jimp.getWidth() / 324
+		ratio = jimp.getWidth() / 720
 	};
 	width = width / ratio;
 	hight = hight / ratio;
@@ -210,6 +210,41 @@ const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
     const cropped = jimp.crop(0, 0, min, max);
     return {
       img: await cropped.scaleToFit(720, 720).getBufferAsync(Jimp.MIME_JPEG),
+      preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
+    };
+}
+
+export const generateProfilePictureFull2 = async(img) => {
+	const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+	const jimp = await read(img)
+	const min = Math.min(jimp.getWidth(), jimp.getHeight())
+	const cropped = jimp.crop(0, 0, jimp.getWidth(), jimp.getHeight())
+	let width = jimp.getWidth(),
+		hight = jimp.getHeight(),
+		ratio;
+	if (width > hight) {
+		ratio = jimp.getWidth() / 1280
+	} else {
+		ratio = jimp.getWidth() / 1280
+	};
+	width = width / ratio;
+	hight = hight / ratio;
+	img = cropped.quality(100).resize(width, hight).getBufferAsync(MIME_JPEG);
+	return {
+		img: await img
+	}
+}
+
+export const generateProfilePictureFP2 = async(buffer) => {
+	const Jimp = require('jimp')
+const { read, MIME_JPEG, RESIZE_BILINEAR } = require('jimp')
+    const jimp = await Jimp.read(buffer);
+    const min = jimp.getWidth();
+    const max = jimp.getHeight();
+    const cropped = jimp.crop(0, 0, min, max);
+    return {
+      img: await cropped.scaleToFit(1280, 1280).getBufferAsync(Jimp.MIME_JPEG),
       preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
     };
 }
