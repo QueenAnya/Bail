a70f26b2f2c046cf1b19a22d9955c767103a3bc6
Teamolduser <jetushackk@usa.com>
2025-01-01 12:04:02 +0530

    Update

--- u.sh
+++ u.sh
@@ -30,5 +30,5 @@ git config --global user.email "jetushackk@usa.com"
 git config --global user.name "Teamolduser"
 git commit -m "Update"
 git branch -M master
- git remote add origin https://github.com/QueenAnya/bail.git"
+ git remote add origin https://github.com/QueenAnya/bail.git
 git push -u origin master
