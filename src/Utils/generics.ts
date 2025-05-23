a041ff4f88a50b196e5482c18aeb6a87620c7a63
Its-me-001 <khushijain2901@mail.com>
2025-05-23 20:46:10 +0530

    #fk

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -35,13 +35,14 @@ export const Browsers: BrowsersMap = {
 	appropriate: (browser) => [ PLATFORM_MAP[platform()] || 'Ubuntu', browser, release() ]
 }
 
+/**
 export const getPlatformId = (browser: string) => {
     const platformType = proto.DeviceProps.PlatformType[browser.toUpperCase()];
     return platformType ? platformType.toString().charCodeAt(0).toString() : '51'; // chrome
 };
+*/
 
-
-export const getPlatformLId = (browser: string) => {
+export const getPlatformId = (browser: string) => {
 	const platformType = proto.DeviceProps.PlatformType[browser.toUpperCase()]
 	return platformType ? platformType.toString() : '1' //chrome
 };
