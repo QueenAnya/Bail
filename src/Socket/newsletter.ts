13b49c571fde62bdbb898e7aa07e9688ba794208
OLDUSER <olduser@esm.apiis.dpdns.org>
2026-01-20 18:26:40 +0530

    098765poiuyt

--- src/Socket/newsletter.ts
+++ src/Socket/newsletter.ts
@@ -2,7 +2,7 @@ import type { NewsletterCreateResponse, SocketConfig, WAMediaUpload } from '../T
 import type { NewsletterMetadata, NewsletterUpdate } from '../Types'
 import { QueryIds, QueryIdd, XWAPaths } from '../Types'
 import { generateProfilePicture } from '../Utils/messages-media'
-import { S_WHATSAPP_NET, getBinaryNodeChild } from '../WABinary'
+import { getBinaryNodeChild } from '../WABinary'
 import { makeGroupsSocket } from './groups'
 import { executeWMexQuery as genericExecuteWMexQuery } from './mex'
 
