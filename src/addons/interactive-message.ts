6a41f74b4a7716eb48ea4702b879ff97cfdf9685
OLDUSER <olduser@esm.apiis.dpdns.org>
2026-04-02 21:48:20 +0530

    #fixedddd0201

--- src/addons/interactive-message.ts
+++ src/addons/interactive-message.ts
@@ -1,6 +1,6 @@
 import { proto } from '../../WAProto/index.js'
 import type { MediaGenerationOptions, WAMediaUpload } from '../Types'
-import { prepareWAMessageMedia } from '../utils/messages'
+import { prepareWAMessageMedia } from '../Utils/messages'
 
 // ─── Input Types ───────────────────────────────────────────────────────────────
 
