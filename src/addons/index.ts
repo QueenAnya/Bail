4ceec617b88ff3c6715bdb2f6929b98536dff422
OLDUSER <olduser@esm.apiis.dpdns.org>
2026-04-11 21:07:53 +0530

    ##################₹##₹₹₹₹₹₹#####

--- src/addons/index.ts
+++ src/addons/index.ts
@@ -37,9 +37,6 @@ export * from './message-search'
 // Message Templates
 export * from './templates'
 
-// Chat Control (Typing, Pinned Messages, Read Receipts)
-export * from './chat-control'
-
 // JID Plotting & LID Support
 export * from './jid-plotting'
 
