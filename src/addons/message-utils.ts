268c8a5cf432650938f0ccd78dbb7055cd0d8e31
OLDUSER <olduser@esm.apiis.dpdns.org>
2026-04-23 10:36:21 +0530

    010101010101010101010101

--- src/addons/message-utils.ts
+++ src/addons/message-utils.ts
@@ -1,12 +1,6 @@
 import { randomBytes } from 'crypto'
 import { proto } from '../../WAProto/index.js'
-import type {
-	AnyMessageContent,
-	MessageGenerationOptions,
-	WAMediaUpload,
-	WAMediaUploadFunction,
-	WAMessage
-} from '../Types'
+import type { WAMediaUpload, WAMediaUploadFunction, WAMessage } from '../Types'
 import { QueryIds, XWAPaths } from '../Types'
 import {
 	generateWAMessage,
